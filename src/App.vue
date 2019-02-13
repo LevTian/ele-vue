3db84e254ec39cd07860296f22c2ef7cc31c220a
LevTian <tyln2013@163.com>
2019-02-13 17:32:00 +0800

    header start

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <mainHeader></mainHeader>
+    <mainHeader :seller="seller"></mainHeader>
     <div class="tab">
       <div class="tab-item"><router-link to="/goods">商品</router-link></div>
       <div class="tab-item"><router-link to="/ratings">评价</router-link></div>
@@ -19,14 +19,17 @@ export default {
   components: {
     mainHeader,
   },
+  data() {
+    return {
+      seller: {}
+    }
+  },
   created() {
     axios.get("/data.json").then((res) => {
       if (200 == res.status && res.data) {
-        console.log(res.data);
-        console.log("res.status", res.status);
-        console.log("res.statusText", res.statusText);
-        console.log("res.headers", res.headers);
-        console.log("res.config", res.config);
+        let data = res.data;
+        console.log(data);
+        this.seller = data.seller;
       }
     }).catch((error) => {
       console.log("出错了", error);
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,13 +1,75 @@
 <template>
-    <div class="header">i am header</div>
+    <div class="header">
+        <div class="content-wrapper">
+            <div class="avatar">
+                <img width="64" height="64" :src="seller.avatar" />
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span class="brand"></span>
+                    <span class="name">{{ seller.name }}</span>
+                </div>
+                <div class="description">
+                    {{ seller.description }}/{{ seller.deliveryTime }}分钟送达
+                </div>
+                <div class="support" v-if="seller.supports"> <!-- 需要加判断，如没有的话在下面取数组0会报错 -->
+                    <span class="icon">{{ seller.supports[0].type }}</span>
+                    <span class="text">{{ seller.supports[0].description }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="bulletin-wrapper"></div>
+        
+    </div>
 </template>
 
 <script>
 export default {
-    name : "Header"
+    name : "Header",
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        seller : Object
+    }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+    @import "../../common/stylus/mixin.styl"
+
+    .header
+        color : #ffffff
+        background : #cccccc
+        .content-wrapper
+            padding : .48rem .24rem .36rem .48rem
+            .avatar
+                display : inline-block
+            .content
+                display : inline-block
+                margin-left : .32rem
+                .title
+                    margin : .4rem 0 .16rem 0
+                    
+                    .brand 
+                        display : inline-block
+                        width : .6rem
+                        height : .32rem
+                        bg-image("images/brand")
+                        background-size : .6rem .32rem
+                        backgroundrepeat : no-repeat
+                    .name
+                        margin-left : .12rem
+                        font-size : .32rem
+                        font-weight : bold
+                        line-height : .36rem
+                        vertical-align : top
+                .description
+                    margin-bottom : .2rem
+                    font-size : .24rem
+                    font-weight : 200
+                    line-height : 24px
 
 </style>
